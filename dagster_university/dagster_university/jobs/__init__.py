a006fab2472fa50e34939b5c97cd4d0330dc2ad5
mbaula <markbaula.1@gmail.com>
2024-09-12 10:47:48 -0400

    Create a weekly partition

--- dagster_university/dagster_university/jobs/__init__.py
+++ dagster_university/dagster_university/jobs/__init__.py
@@ -2,6 +2,11 @@ from dagster import define_asset_job, AssetSelection
 
 trips_by_week = AssetSelection.assets(["trips_by_week"])
 
+trip_update_job = define_asset_job(
+    name="trip_update_job",
+    selection=AssetSelection.all() - trips_by_week
+)
+
 weekly_update_job = define_asset_job(
     name="weekly_update_job",
     selection=trips_by_week,
--- dagster_university/dagster_university/schedules/__init__.py
+++ dagster_university/dagster_university/schedules/__init__.py
@@ -1,6 +1,11 @@
 from dagster import AssetSelection, ScheduleDefinition
 
-from ..jobs import weekly_update_job
+from ..jobs import trip_update_job, weekly_update_job
+
+trip_update_schedule = ScheduleDefinition(
+    job=trip_update_job,
+    cron_schedule="0 0 5 * *", # every 5th of the month at midnight
+)
 
 weekly_update_schedule = ScheduleDefinition(
     job=weekly_update_job,
